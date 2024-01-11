41e5ed33a2827a0a1b6202f2f33dfbcb65dd4340
Edvin Larsson <edvinlarsson0110@gmail.com>
2024-01-11 13:17:42 +0000

    Summernote installation and setup

--- mainpage/admin.py
+++ mainpage/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Restaurant, Booking
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Restaurant)
+class RestaurantAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'slug', 'status')
+    search_fields = ['name']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('name',)}
+    summernote_fields = ('about',)
 
 # Register your models here.
-admin.site.register(Restaurant)
 admin.site.register(Booking)
--- mainpage/models.py
+++ mainpage/models.py
@@ -7,17 +7,21 @@ STATUS = ((0, "Pending"), (1, "Published"))
 # Create your models here.
 class Restaurant(models.Model):
     name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=100, unique=True, null=True)
     food_type = models.CharField(max_length=20)
     city = models.CharField(max_length=25)
     about = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
+    
+    # These attributes are for data handling and administrative operations
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="restaurant_posts"
+        User, on_delete=models.CASCADE, related_name="restaurant_posts",
+        default=User
     )
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
-        ordering = ["city"]
+        ordering = ["city", "name"]
 
     def __str__(self):
         return self.name
